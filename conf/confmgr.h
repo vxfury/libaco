ea2c9f19a5d287e06f5f98e4e416f847e95b03d6
Kiran <kiran.nowak@gmail.com>
2022-02-20 02:33:57 +0800

    update

--- conf/confmgr.h
+++ conf/confmgr.h
@@ -6,111 +6,324 @@
 #include <atomic>
 #include <type_traits>
 #include <unordered_map>
+#include <mutex>
+#include <shared_mutex>
+#include <sstream>
+#include <stdio.h>
+
+#define TRACE(...) printf("%s:%d ", __FILE__, __LINE__), printf(__VA_ARGS__), printf("\n")
 
 namespace conf
 {
-class spinlock {
-  public:
-    spinlock() = default;
-    spinlock(const spinlock &) = delete;
-    spinlock &operator=(const spinlock &) = delete;
+namespace conv
+{
+static inline std::string trim(const std::string &s)
+{
+    std::string tmp = s;
+    tmp.erase(tmp.begin(), std::find_if(tmp.begin(), tmp.end(), [](unsigned char ch) {
+                  return !std::isspace(ch);
+              }));
 
-    void lock()
-    {
-        bool expected = false;
-        while (!flag.compare_exchange_strong(expected, true)) {
-            expected = false;
+    tmp.erase(std::find_if(tmp.rbegin(), tmp.rend(),
+                           [](unsigned char ch) {
+                               return !std::isspace(ch);
+                           })
+                  .base(),
+              tmp.end());
+    return tmp;
+}
+
+int derive(const std::string &from, bool &to)
+{
+    std::string tmp = trim(from);
+    const char *p = tmp.c_str();
+
+    char l = *p | ' ';
+    if ((l == 't' && strncmp(++p, "rue\0", 4) == 0) || strncmp(p, "1\0", 2) == 0) {
+        to = true;
+    } else if ((l == 'f' && strncmp(++p, "alse\0", 5) == 0) || strncmp(p, "0\0", 2) == 0) {
+        to = false;
+    } else {
+        return -EINVAL;
+    }
+
+    return 0;
+}
+
+int derive(const std::string &from, std::string &to)
+{
+    to = from;
+    return 0;
+}
+
+template <typename T, typename std::enable_if<std::is_integral<T>::value>::type * = nullptr>
+int derive(const std::string &from, T &value)
+{
+    std::string tmp = trim(from);
+    if (tmp.empty()) {
+        return -EINVAL;
+    }
+
+    bool negative = false;
+    if (tmp[0] == '-') {
+        negative = true;
+    }
+    unsigned long long v = std::stoull(negative ? tmp.substr(1) : tmp, nullptr, 0);
+
+    value = static_cast<T>(v) * (negative ? -1 : 1);
+
+    return 0;
+}
+
+template <typename To>
+int derive(const std::string &from, std::vector<To> &to)
+{
+    std::string tmp = trim(from);
+    if (tmp.empty()) {
+        to.clear();
+        TRACE("[from-string] Warning: Empty");
+        return 0;
+    }
+    if (tmp[0] == '[' || tmp[0] == '{') {
+        tmp = tmp.substr(1);
+    }
+    if (tmp[tmp.size() - 1] == ']' || tmp[tmp.size() - 1] == '}') {
+        tmp = tmp.erase(tmp.size() - 1, 1);
+    }
+
+    std::stringstream ss(tmp);
+    while (ss.good()) {
+        std::getline(ss, tmp, ',');
+
+        To item;
+        if (int err = derive(tmp, item); err != 0) {
+            TRACE("[from-string] Error(%d): derive failed(from = %s)", err, tmp.c_str());
+            return err;
+        }
+        to.push_back(item);
+    }
+
+    return 0;
+}
+
+template <typename Key, typename Value>
+int derive(const std::string &from, std::unordered_map<Key, Value> &to)
+{
+    std::string tmp = trim(from);
+    if (tmp.empty()) {
+        to.clear();
+        return 0;
+    }
+    std::stringstream ss(tmp);
+    while (ss.good()) {
+        std::string blk;
+        std::getline(ss, blk, ';');
+        {
+            trim(blk);
+            auto it = blk.find(':');
+            if (it == std::string::npos) {
+                return -EINVAL;
+            }
+            Key key;
+            if (int err = derive(blk.substr(0, it), key); err == 0) {
+                Value val;
+                if (int err = derive(blk.substr(it + 1), val); err != 0) {
+                    TRACE("[from-string] Error(%d): derive failed(from = %s)", err,
+                          blk.substr(it + 1).c_str());
+                    return err;
+                }
+                to[key] = val;
+            }
         }
     }
 
-    void unlock()
-    {
-        flag.store(false);
+    return 0;
+}
+
+template <typename T>
+int derive(const std::string &from, std::vector<std::vector<T>> &to)
+{
+    std::string tmp = trim(from);
+    if (tmp.empty()) {
+        to.clear();
+        return 0;
+    }
+    const char *s = tmp.c_str(), *p = s;
+    const char *e = tmp.c_str() + tmp.length();
+
+    int depth = 0;
+    while (p <= e) {
+        if (*p == '[' || *p == '{') {
+            if (depth == 0) {
+                s = ++p;
+            } else {
+                s = p++;
+            }
+            depth++;
+            do {
+                if (*p == '[' || *p == '{') {
+                    depth++;
+                } else if (*p == ']' || *p == '}') {
+                    depth--;
+                }
+                p++;
+            } while (*p != '\0' && depth != 1);
+
+            std::vector<T> vec;
+            if (int err = derive(std::string(s, p++ - s), vec); err != 0) {
+                TRACE("[from-string] Error(%d): derive failed(from = %s)", err,
+                      std::string(s, p - s).c_str());
+                return err;
+            }
+            to.push_back(vec);
+
+            while (*p == ',' || *p == ' ') {
+                p++;
+            }
+        } else {
+            p++;
+        }
     }
 
-  private:
-    std::atomic<bool> flag = ATOMIC_VAR_INIT(false);
-};
+    return 0;
+}
 
-class lockguard {
-  public:
-    lockguard(spinlock &lock) : lock_(lock)
-    {
-        lock_.lock();
+template <typename T, typename std::enable_if<!std::is_integral<T>::value>::type * = nullptr>
+int derive(const std::string &text, T &value)
+{
+    std::stringstream in(text);
+    in >> value;
+    if (!in) {
+        return -EINVAL;
+    }
+    return 0;
+}
+
+template <typename From>
+int derive(const From &from, std::string &to)
+{
+    std::stringstream ss;
+    ss << from;
+    to = ss.str();
+
+    return 0;
+}
+
+int derive(const bool &from, std::string &to)
+{
+    to = from ? "true" : "false";
+    return 0;
+}
+
+template <typename T>
+int derive(const std::vector<T> &from, std::string &to)
+{
+    to = "[";
+    const std::string delim = ", ";
+    for (auto const &e : from) {
+        if (std::string str; derive(e, str) == 0) {
+            to += str + delim;
+        } else {
+            return -EINVAL;
+        }
+    }
+    if (from.size() != 0) {
+        to.erase(to.size() - delim.size(), delim.size());
+    }
+    to += "]";
+
+    return 0;
+}
+
+template <typename K, typename V>
+int derive(const std::unordered_map<K, V> &from, std::string &to)
+{
+    for (auto const &e : from) {
+        std::string key, val;
+        if (int err = derive(e.first, key); err != 0) {
+            TRACE("[to-string] Error(%d): derive failed", err);
+            return err;
+        }
+        if (int err = derive(e.second, val); err != 0) {
+            TRACE("[to-string] Error(%d): derive failed", err);
+            return err;
+        }
+        to += key + ": " + val + "; ";
+    }
+    if (to.size() >= 2) {
+        to.erase(to.size() - 2, 2); // pop last delimeters
     }
 
-    ~lockguard()
-    {
-        lock_.unlock();
-    }
+    return 0;
+}
 
-  private:
-    spinlock &lock_;
-};
+template <typename To>
+int derive(const std::string &from, To &to)
+{
+    TRACE("[from-string] Error(%d): Not Implemented", -ENOSYS);
+    return -ENOSYS;
+}
+} // namespace conv
 
 template <typename Key, typename... Values>
 class manager {
   public:
-    using key_type = Key;
-    using value_type = std::variant<Values...>;
+    enum level { DEFAULT = 0, WORKSPACE, PULL_STAGE, PUSH_STAGE, NUMBER };
+    using map_type = std::unordered_map<Key, std::variant<Values...>>;
+
+#define WR_LOCKGUARD(level) std::lock_guard<std::shared_mutex> guard_##level(mutexs[level])
+#define RD_LOCKGUARD(level) std::shared_lock<std::shared_mutex> guard_##level(mutexs[level])
 
     template <typename T = void>
     bool has(const Key &key)
     {
-        lockguard guard(workspace_lock);
-
-        if constexpr (std::is_same_v<T, void>) {
-            return workspace.count(key) != 0;
-        } else {
-            if (workspace.count(key)) {
-                if (std::get_if<T>(workspace.at(key)) != nullptr) {
-                    return true;
-                }
-            }
-            return false;
-        }
+        RD_LOCKGUARD(WORKSPACE);
+        return has<T>(WORKSPACE, key);
     }
 
-    template <typename T>
-    int get(const Key &key, T &value, bool try_pull)
+    template <typename Value, typename Store = Value>
+    int get(const Key &key, Value &value, bool try_pull = true)
     {
         if (try_pull) {
             std::set<Key> keys = {key};
             if (int err = pull(relevant_keys(key, keys)); err != 0) {
+                TRACE("Error(%d): pull failed", err);
                 return err;
             }
         }
 
         {
-            lockguard guard(workspace_lock);
-            if (workspace.count(key) > 0) {
-                if (const T *p = std::get_if<T>(&workspace.at(key)); p != nullptr) {
-                    value = *p;
-                    return 0;
-                } else {
-                    return -EINVAL;
-                }
+            RD_LOCKGUARD(PUSH_STAGE);
+            if (get<Value, Store>(PUSH_STAGE, key, value) == 0) {
+                return 0;
             }
-            return -ENOENT;
+        }
+
+        {
+            RD_LOCKGUARD(WORKSPACE);
+            return get<Value, Store>(WORKSPACE, key, value);
         }
     }
 
-    template <typename T>
-    T get(const Key &key, const T &defval, bool try_pull = true)
+    template <typename Value, typename Store = Value>
+    Value get_as(const Key &key, const Value &defval, bool try_pull = true)
     {
-        T val;
-        if (get(key, val, try_pull) != 0) {
+        Value val;
+        if (get<Value, Store>(key, val, try_pull) != 0) {
             val = defval;
         }
         return val;
     }
 
-    template <typename T>
-    int set(const Key &key, const T &value, bool try_push = true)
+    template <typename Value, typename Store = Value>
+    int set(const Key &key, const Value &value, bool try_push = true)
     {
         {
-            lockguard guard(push_stage_lock);
-            push_stage[key] = value;
+            WR_LOCKGUARD(PUSH_STAGE);
+            if (int err = set<Store, Value>(PUSH_STAGE, key, value); err != 0) {
+                TRACE("Error(%d): set failed", err);
+                return err;
+            }
         }
 
         if (try_push) {
@@ -123,22 +336,23 @@ class manager {
 
     virtual int push(const std::set<Key> &keys)
     {
-        lockguard guard_stage(push_stage_lock);
+        WR_LOCKGUARD(PUSH_STAGE);
 
         /* push to storage */
         {
-            if (int err = sync_push(keys, push_stage); err != 0) {
+            if (int err = sync_push(keys, maps[PUSH_STAGE]); err != 0) {
+                TRACE("Error(%d): sync failed", err);
                 return err;
             }
         }
 
         /* swap with stage */
         {
-            lockguard guard(workspace_lock);
+            WR_LOCKGUARD(WORKSPACE);
             for (auto &key : keys) {
-                if (push_stage.count(key)) {
-                    workspace[key] = push_stage[key];
-                    push_stage.erase(key);
+                if (maps[PUSH_STAGE].count(key)) {
+                    maps[WORKSPACE][key] = maps[PUSH_STAGE][key];
+                    maps[PUSH_STAGE].erase(key);
                 }
             }
         }
@@ -148,22 +362,23 @@ class manager {
 
     virtual int pull(const std::set<Key> &keys)
     {
-        lockguard guard_stage(pull_stage_lock);
+        WR_LOCKGUARD(PULL_STAGE);
 
         /* pull from storage */
         {
-            if (int err = sync_pull(keys, pull_stage); err != 0) {
+            if (int err = sync_pull(keys, maps[PULL_STAGE]); err != 0) {
+                TRACE("Error(%d): sync failed", err);
                 return err;
             }
         }
 
         /* swap with stage */
         {
-            lockguard guard(workspace_lock);
+            WR_LOCKGUARD(WORKSPACE);
             for (auto &key : keys) {
-                if (pull_stage.count(key)) {
-                    workspace[key] = pull_stage[key];
-                    pull_stage.erase(key);
+                if (maps[PULL_STAGE].count(key)) {
+                    maps[WORKSPACE][key] = maps[PULL_STAGE][key];
+                    maps[PULL_STAGE].erase(key);
                 }
             }
         }
@@ -172,45 +387,109 @@ class manager {
     }
 
   protected:
+    template <typename From, typename To>
+    int derive(const From &from, To &to)
+    {
+        if constexpr (std::is_same_v<From, std::string> || std::is_same_v<To, std::string>) {
+            return conv::derive(from, to);
+        }
+
+        return -ENOSYS;
+    }
+
     virtual std::set<Key> &relevant_keys(const Key &, std::set<Key> &keys)
     {
         return keys;
     }
 
-    virtual int sync_pull(const std::set<Key> &, std::unordered_map<Key, value_type> &)
+    virtual int sync_pull(const std::set<Key> &, map_type &)
     {
         return 0;
     }
 
-    virtual int sync_push(const std::set<Key> &, const std::unordered_map<Key, value_type> &)
+    virtual int sync_push(const std::set<Key> &, const map_type &)
     {
         return 0;
     }
 
-    spinlock workspace_lock;
-    spinlock pull_stage_lock;
-    spinlock push_stage_lock;
-    std::unordered_map<Key, value_type> workspace;
-    std::unordered_map<Key, value_type> pull_stage;
-    std::unordered_map<Key, value_type> push_stage;
+    template <typename Value, typename Store = Value>
+    int get(int level, const Key &key, Value &value)
+    {
+        const auto &map = maps[level];
+        if (map.count(key) > 0) {
+            if (const Store *p = std::get_if<Store>(&map.at(key)); p != nullptr) {
+                if constexpr (std::is_same_v<Value, Store>) {
+                    value = *p;
+                    return 0;
+                } else {
+                    return derive(*p, value);
+                }
+            } else {
+                return -EINVAL;
+            }
+        }
+        return -ENOENT;
+    }
+
+    template <typename Store, typename Value = Store>
+    int set(int level, const Key &key, const Value &value)
+    {
+        auto &map = maps[level];
+        if constexpr (std::is_same_v<Store, Value>) {
+            map[key] = value;
+        } else {
+            Store to;
+            if (int err = derive(value, to); err != 0) {
+                TRACE("Error(%d): derive failed", err);
+                return err;
+            }
+            map[key] = to;
+        }
+
+        return 0;
+    }
+
+    template <typename Value = void, typename Store = Value>
+    bool has(int level, const Key &key)
+    {
+        const auto &map = maps[level];
+        if constexpr (std::is_same_v<Value, void>) {
+            return map.count(key) != 0;
+        } else {
+            if (map.count(key)) {
+                if (const Store *p = std::get_if<Store>(map.at(key)); p != nullptr) {
+                    if constexpr (std::is_same_v<Value, Store>) {
+                        return true;
+                    } else {
+                        Value to;
+                        return derive(*p, to) == 0;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    map_type maps[level::NUMBER];
+    std::shared_mutex mutexs[level::NUMBER];
 };
 
 template <typename Key = std::string>
-class DistributedManager : public manager<Key, bool, uint32_t, uint64_t, std::string> {
+class DistributedManager : public manager<Key, bool, int, uint32_t, uint64_t, std::string> {
   public:
-    using value_type = typename manager<Key, bool, uint32_t, uint64_t, std::string>::value_type;
+    using map_type = std::unordered_map<Key, std::variant<bool, int, uint32_t, uint64_t, std::string>>;
     DistributedManager(std::string repo, std::string branch)
         : repo_(std::move(repo)), branch_(std::move(branch))
     {
     }
 
   protected:
-    virtual int sync_pull(const std::set<Key> &, std::unordered_map<Key, value_type> &) override
+    virtual int sync_pull(const std::set<Key> &, map_type &) override
     {
         return 0;
     }
 
-    virtual int sync_push(const std::set<Key> &, const std::unordered_map<Key, value_type> &) override
+    virtual int sync_push(const std::set<Key> &, const map_type &) override
     {
         return 0;
     }
@@ -221,18 +500,18 @@ class DistributedManager : public manager<Key, bool, uint32_t, uint64_t, std::st
 };
 
 template <typename Key = std::string>
-class FileManager : public manager<Key, bool, uint32_t, uint64_t, std::string> {
+class FileManager : public manager<Key, bool, int, uint32_t, uint64_t, std::string> {
   public:
-    using value_type = typename manager<Key, bool, uint32_t, uint64_t, std::string>::value_type;
+    using map_type = std::unordered_map<Key, std::variant<bool, int, uint32_t, uint64_t, std::string>>;
     FileManager(std::string path) : path_(std::move(path)) {}
 
   protected:
-    virtual int sync_pull(const std::set<Key> &, std::unordered_map<Key, value_type> &) override
+    virtual int sync_pull(const std::set<Key> &, map_type &) override
     {
         return 0;
     }
 
-    virtual int sync_push(const std::set<Key> &, const std::unordered_map<Key, value_type> &) override
+    virtual int sync_push(const std::set<Key> &, const map_type &) override
     {
         return 0;
     }
--- conf/test.cc
+++ conf/test.cc
@@ -1,3 +1,4 @@
+#include <vector>
 #include <iostream>
 #include "confmgr.h"
 
@@ -5,10 +6,44 @@ int main()
 {
     conf::DistributedManager mgr("repo", "master");
 
-    mgr.set("key1", true);
-    if (mgr.has("key1")) {
-        std::cout << "has: key1" << std::endl;
+    // native types
+    {
+        bool val1 = true;
+        int val2 = 100;
+        std::string val3 = "string";
+
+        mgr.set("key1", val1);
+        mgr.set("key2", val2);
+        mgr.set("key3", val3);
+
+        if (mgr.has("key1")) {
+            std::cout << "has: key1" << std::endl;
+        }
+        std::cout << "key1: " << mgr.get_as<bool>("key1", false) << std::endl;
+        std::cout << "key2: " << mgr.get_as<int>("key2", 0) << std::endl;
     }
-    std::cout << "key1: " << mgr.get<std::string>("key1", "defval(incorrect type)") << std::endl;
-    std::cout << "key2: " << mgr.get<std::string>("key2", "defval(key not exists)") << std::endl;
+
+    // containers
+    {
+        std::vector<int> val4{1, 2, 4, 8, 16};
+        std::vector<std::vector<int>> val5 = {{1, 2, 3}, {4, 5, 6}, {8, 0, 10, 12}};
+        std::unordered_map<std::string, std::vector<int>> val6 = {
+            {"key1", {1, 2, 3}},
+            {"key2", {4, 5, 6}},
+            {"key3", {8, 0, 10, 12}},
+        };
+
+        mgr.set<std::vector<int>, std::string>("key4", val4);
+        mgr.set<std::vector<std::vector<int>>, std::string>("key5", val5);
+        mgr.set<std::unordered_map<std::string, std::vector<int>>, std::string>("key6", val6);
+
+        std::cout << "key4: " << mgr.get_as<std::string>("key4", "") << std::endl;
+        std::cout << "key4: ";
+        for (auto v : mgr.get_as<std::vector<int>, std::string>("key4", {})) {
+            std::cout << v << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
